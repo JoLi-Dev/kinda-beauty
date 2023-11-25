e0e7a28bbdb2fa1b7326c885ee78edbbdd529517
Joanne Lindsay <jolinday4@gmail.com>
2023-11-26 10:45:22 +1100

    added header main banner and footer

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+/*const header = document.querySelector('header')
+const main = document.querySelector('main')
+const par = document.querySelector('p')
+const footer = document.querySelector('footer')
 
-document.querySelector("body").appendChild(h2);
+const h3 = document.createElement('h3')
+h3.innerText = 'Some h3 heading'
+header.append(h3)
+
+function handleClick(event){
+   console.log(event)
+   alert("I've been clicked!")
+}
+
+//header.onclick = (event) => {
+    //console.log(event)
+    //alert("I've been clicked!")
+//}
+
+//header.addEventListener('click', () => { alert("I've been clicked!")})//
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const ul = document.querySelector('ul')
+    ul.innerHTML += `<li>${firstName.value} ${lastName.value}</li>`
+
+    form.reset()
+})*/
+
