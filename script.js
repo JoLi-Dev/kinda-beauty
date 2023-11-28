8d81fb84b3698dbbcf0559f474a01b0d0579cdb7
Joanne Lindsay <jolinday4@gmail.com>
2023-11-28 19:31:16 +1100

    added js get data for api

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-/*const header = document.querySelector('header')
-const main = document.querySelector('main')
-const par = document.querySelector('p')
-const footer = document.querySelector('footer')
-
-const h3 = document.createElement('h3')
-h3.innerText = 'Some h3 heading'
-header.append(h3)
-
-function handleClick(event){
-   console.log(event)
-   alert("I've been clicked!")
+async function getData(){
+    const response = await fetch ("http://makeup-api.herokuapp.com/api/v1/products.json")
+    const products = await response.json();
+    console.log(products);
+    renderMakeupList(products.array)
 }
-
-//header.onclick = (event) => {
-    //console.log(event)
-    //alert("I've been clicked!")
-//}
-
-//header.addEventListener('click', () => { alert("I've been clicked!")})//
-
-const form = document.querySelector('form')
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const ul = document.querySelector('ul')
-    ul.innerHTML += `<li>${firstName.value} ${lastName.value}</li>`
-
-    form.reset()
-})*/
-
+function renderMakeupList(fullMakeupList) {
+    fullMakeupList.forEach(products => {
+        const ul = document.querySelector ('ul')
+        const li = document.querySelector ('li')
+        li.innerText = products.name
+        ul.append(li)
+    });
+}
+getData();
+    
